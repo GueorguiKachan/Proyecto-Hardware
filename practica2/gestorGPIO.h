e967109880b65dae7dd715f034d07efd4d4fb2bd
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-10-22 20:13:44 +0200

    Create gestorGPIO.h

---
+++ practica2/gestorGPIO.h
@@ -0,0 +1,5 @@
+void GPIO_iniciar();
+int GPIO_leer(int bit_inicial,int num_bits);
+int GPIO_escribir(int bit_inicial,int num_bits,int valor);
+void GPIO_marcar_entrada(int bit_inicial,int num_bits);
+void GPIO_marcar_salida(int bit_inicial,int num_bits);
