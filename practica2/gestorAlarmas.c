694a87c12ad1ada815874187307b6bd1199792f5
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-10-22 18:50:07 +0200

    Update gestorAlarmas.c

--- practica2/gestorAlarmas.c
+++ practica2/gestorAlarmas.c
@@ -1,16 +1,38 @@
 #include <LPC210X.H>                            // LPC21XX Peripheral Registers
 #include "gestorAlarmas.h"
 
+static volatile unsigned int timer0_int_count = 0;
+static volatile unsigned int timer1_int_count = 0;
+
+void timer0_ISR (void) __irq;    // Generate Interrupt
+void timer1_ISR (void) __irq;    // Generate Interrupt
+
 void temporizador_iniciar(){
+  // VPBDIV_Val = 0x1 --> Esto creo que hace que el PCLK sea el mismo que el CCLK, así el reloj del contador va más rápido. Sección 3.12 del manual LPC
   T1CTCR = 0x0; // Selecciona modo contador, que cuenta al llegar un flanco ascendente del PCLK
-  T1TCR = 0x02 // Bit 1: 1, Bit 0: 0, hace reset del contador y el PCLK. No activa el contador
-  T1PR: = 0x0 // Cuando el Prescale Counter Register incremente en 1, el Time Counter también lo hará
+  T1TCR = 0x02; // Bit 1: 1, Bit 0: 0, hace reset del contador y el PCLK. No activa el contador
+  T1PR: = 0x0; // Cuando el Prescale Counter Register incremente en 1, el Time Counter también lo hará
+  T1MR0 = 1; // Con esto creo que salta una interrupción cuando el TC sea igual a 1, que ocurrirá cada micro 
+	
+  VICVectAddr0 = (unsigned long)timer1_ISR;          // set interrupt vector in 0
+    // 0x20 bit 5 enables vectored IRQs. 
+		// 4 is the number of the interrupt assigned. Number 4 is the Timer 0 (see table 40 of the LPC2105 user manual  
+  VICVectCntl0 = 0x20 | 5; // Página 48 del manual, tabla 40. Número asignado a Timer1                   
+  VICIntEnable = VICIntEnable | 0x00000010;                  // Enable Ti
 }
 void temporizador_empezar(){
    T1TCR = 1; // Iniciar el contador 
 }
 
-void timer0_ISR (void) __irq;    // Generate Interrupt 
+void timer1_ISR (void) __irq {
+    timer1_int_count++;
+    T0IR = 1;                              // Clear interrupt flag
+    VICVectAddr = 0;                            // Acknowledge Interrupt
+}
+
+unsigned int temporizador_leer(){
+	return timer1_int_count;
+}
 
 
 /* Setup the Timer Counter 0 Interrupt */
