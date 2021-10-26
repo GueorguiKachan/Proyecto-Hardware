a1ff6b0a5a8ff442557816b825d7beb16ce8dde0
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-10-26 09:40:02 +0200

    Update gestorGPIO.c

--- practica2/gestorGPIO.c
+++ practica2/gestorGPIO.c
@@ -8,45 +8,51 @@ void GPIO_iniciar(){
 	PINSEL1 = 0x00000000;
 }
 int GPIO_leer(int bit_inicial,int num_bits){
+	int i;
 	int result = 0;
 	uint32_t aux = IOPIN;
 	aux = aux / pow(2,bit_inicial);
-	for (int i = 0; i< num_bits; i++){
+	for (i = 0; i< num_bits; i++){
 		result = result + aux %2;
 		aux = aux / 2;
 	}
+	return result;
 }
 int GPIO_escribir(int bit_inicial,int num_bits,int valor){
 	uint32_t aux = 0;
-	for (int i = 0; i < num_bits; i++){
+	int i;
+	for (i = 0; i < num_bits; i++){
 		aux = aux * 2;
 		if (valor % 2 == 1){
 			aux = aux + 1;
 		}
 		valor = valor / 2;
 	}
-	aux = aux * pow(2,bit_inicial);
+	aux = aux << bit_inicial;
 	IOPIN = IOPIN | aux;
+	return aux;
 }	
 
 void GPIO_marcar_entrada(int bit_inicial,int num_bits){
-	uint32_t aux = 0;
+	uint32_t aux = 0x00000000;
 	uint32_t aux2 = 0xffffffff;
-	for (int i = 0; i < num_bits; i++){
+	int i;
+	for (i = 0; i < num_bits; i++){
 		aux = aux * 2;
 		aux = aux + 1;
 	}
-	aux = aux * pow(2,bit_inicial);
+	aux = aux << bit_inicial;
 	aux = aux ^ aux2;
-	IOPIN = IOPIN & aux;
+	IODIR = IODIR & aux;
 }
 
 void GPIO_marcar_salida(int bit_inicial,int num_bits){
 	uint32_t aux = 0;
-	for (int i = 0; i < num_bits; i++){
+	int i;
+	for (i = 0; i < num_bits; i++){
 		aux = aux * 2;
 		aux = aux + 1;
 	}
-	aux = aux * pow(2,bit_inicial);
-	IOPIN = IOPIN | aux;
+	aux = aux << bit_inicial;
+	IODIR = IODIR | aux;
 }
