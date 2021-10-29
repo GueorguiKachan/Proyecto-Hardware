e453656263b1c601f83e4844778b9f290f306966
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-10-29 14:07:28 +0200

    Update gestorGPIO.c

--- practica2/gestorGPIO.c
+++ practica2/gestorGPIO.c
@@ -9,32 +9,29 @@ void GPIO_iniciar(){
 	PINSEL0 = 0x00000000; 
 	PINSEL1 = 0x00000000;
 	
-Gestor_Pulsacion_Init();
+	Gestor_Pulsacion_Init();
 }
 int GPIO_leer(int bit_inicial,int num_bits){
-	int i;
-	int result = 0;
-	uint32_t aux = IOPIN;
-	aux = aux / pow(2,bit_inicial);
-	for (i = 0; i< num_bits; i++){
-		result = result + aux %2;
-		aux = aux / 2;
-	}
-	return result;
+	//Desplazo el trozo de IOPIN a leer hasta el bit 0
+	uint32_t IOPIN_desplazado_bit_inicial = IOPIN / pow(2,bit_inicial);
+	//Preparo la mascara de longitud num_bits
+	uint32_t mascara_num_bits = pow(2,num_bits) - 1;
+	
+	return IOPIN_desplazado_bit_inicial & mascara_num_bits;
 }
 int GPIO_escribir(int bit_inicial,int num_bits,int valor){
 	uint32_t aux = 0;
 	int i;
+	//Preparo el valor que entra en num_bits bits (el valor 3 no entra en 1 bit, por lo que en aux preparo el valor 1)
 	for (i = 0; i < num_bits; i++){
-		aux = aux * 2;
-		if (valor % 2 == 1){
-			aux = aux + 1;
-		}
+		aux = aux + pow(2*(valor%2),i);
 		valor = valor / 2;
 	}
-	aux = aux << bit_inicial;
-	IOPIN = IOPIN | aux;
+	//Pongo en IOPIN la mascara aux desplazada hasta el bit_inicial
+	IOPIN = aux << bit_inicial;
+	//Devuelvo el valor de aux
 	return aux;
+	
 }	
 
 void GPIO_marcar_entrada(int bit_inicial,int num_bits){
@@ -46,6 +43,7 @@ void GPIO_marcar_entrada(int bit_inicial,int num_bits){
 		aux = aux + 1;
 	}
 	aux = aux << bit_inicial;
+	// ^ -> xor
 	aux = aux ^ aux2;
 	IODIR = IODIR & aux;
 }
