3d20aa59b6473bfcd63bd75c0e9d263175c2dc4b
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-10-29 12:42:56 +0200

    Update Gestor_Energia.c

--- practica2/Gestor_Energia.c
+++ practica2/Gestor_Energia.c
@@ -9,7 +9,8 @@ int Idle_Mode = 1;
 
 void PM_power_down (void)  {
   EXTWAKE = EINT1_2; // Bits 1 y 2 a 1 para que EINT1 y EINT2 despierten al procesador
-	PCON |= Power_Down_Mode; // Modo power-down
+  PCON |= Power_Down_Mode; // Modo power-down
+  switch_to_PLL() // Función que habrá que meter en el startup para volver a poner en modo PLL
 }
 
 void PM_idle(void){
