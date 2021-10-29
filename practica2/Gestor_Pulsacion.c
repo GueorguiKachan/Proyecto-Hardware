9e1f00e838b179b251c376f0e08bb615e865d270
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-10-29 12:21:38 +0200

    Update Gestor_Pulsacion.c

--- practica2/Gestor_Pulsacion.c
+++ practica2/Gestor_Pulsacion.c
@@ -2,7 +2,8 @@
 #include <LPC210X.H>    
 static volatile int nueva_pulsacion_eint1 =0;
 static volatile int nueva_pulsacion_eint2 =0;
-
+const int posicion_eint1 = 0x00008000;
+const int posicion_eint2 = 0x00010000;
 
 
 int button_nueva_pulsacion_1(){
@@ -33,10 +34,20 @@ void eint2_ISR (void) __irq {
 
 
 void Gestor_Pulsacion_Init(){
+	PINSEL0 = 0xa0000000; //a activa eint1 y eint2
+	PINSEL1 = 0x00000000;
 	
 	EXTMODE = EXTMODE | 0xF9; //set bit 1 and bit 2 to 0 -> eint1 is lvl-sensitive and eint2 is lvl-sensitive
 	EXTPOLAR = EXTPOLAR | 0xF9;  //set bit 1 and bit 2 to 0 -> eint1 is low-active and eint2 is low-active
 
+	//Activa la posicion 14 y 15 del VIC para las interrupciones 15(eint1) y 16(eint2)
+	VICVectCntl14 = 0x20 | 15; 
+	VICVectCntl15 = 0x20 | 16;  
+	
+	//Asigna las rutinas de excepcion
+	VICVectAddr14= (unsigned long)eint1_ISR;
+	VICVectAddr15= (unsigned long)eint2_ISR;
+	
 	//Activa las solicitud de interrupcion por eint1 (15) y eint2 (16)
 	VICIntEnable = VICIntEnable | posicion_eint1;
 	VICIntEnable = VICIntEnable | posicion_eint2;
