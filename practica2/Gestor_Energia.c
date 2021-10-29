921649af4be7703d681369eecfeda6b519c3153e
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-10-29 12:36:03 +0200

    Update Gestor_Energia.c

--- practica2/Gestor_Energia.c
+++ practica2/Gestor_Energia.c
@@ -1,12 +1,18 @@
 #include <LPC210x.H>                       /* LPC210x definitions */
+#include "Gestor_Energia.h"
 // Set the processor into power down state 
 // The watchdog cannot wake up the processor from power down
+
+int EINT1_2 = 6;
+int Power_Down_Mode = 2;
+int Idle_Mode = 1;
+
 void PM_power_down (void)  {
-  EXTWAKE = 6; // Bits 1 y 2 a 1 para que EINT1 y EINT2 despierten al procesador
-	PCON |= 0x02; // Modo power-down
+  EXTWAKE = EINT1_2; // Bits 1 y 2 a 1 para que EINT1 y EINT2 despierten al procesador
+	PCON |= Power_Down_Mode; // Modo power-down
 }
 
 void PM_idle(void){
 
-	PCON |= 0x01; // Modo idle
+	PCON |= Idle_Mode; // Modo idle
 }
