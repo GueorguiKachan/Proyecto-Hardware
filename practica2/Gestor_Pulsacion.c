a3c2d9b8b0f8026e669ea0e26c846ea3df3614c4
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-11-05 13:06:33 +0100

    Update Gestor_Pulsacion.c

--- practica2/Gestor_Pulsacion.c
+++ practica2/Gestor_Pulsacion.c
@@ -1,9 +1,13 @@
 #include "Gestor_pulsacion.h"
-#include <LPC210X.H>    
+#include <LPC210X.H>   
 static volatile int nueva_pulsacion_eint1 =0;
 static volatile int nueva_pulsacion_eint2 =0;
+static volatile unsigned int eint1_count = 0;
+static volatile unsigned int eint2_count = 0;
 const int posicion_eint1 = 0x00008000;
 const int posicion_eint2 = 0x00010000;
+static volatile char eint1_estado = 'N';
+static volatile char eint2_estado = 'N';
 
 
 int button_nueva_pulsacion_1(){
@@ -22,20 +26,76 @@ void button_clear_nueva_pulsacion_2(){
   nueva_pulsacion_eint2 = 0;
 }
 
+void eint1_comprobar(){
+	//si el boton no esta pulsado
+	EXTINT = EXTINT | 2;
+	if ((VICRawIntr & posicion_eint1) != posicion_eint1){ 
+		VICVectAddr = 0;
+		button_clear_nueva_pulsacion_1();
+		VICIntEnable = VICIntEnable | posicion_eint1;
+	}else{
+	eint1_comprobar();}
+	//sino volver a pedir una alarma de 100ms
+}
+
+void eint2_comprobar(){
+	//si el boton no esta pulsado
+	EXTINT = EXTINT | 2;
+	if ((VICRawIntr & posicion_eint2) != posicion_eint2){ 
+		
+		VICVectAddr = 0;
+		button_clear_nueva_pulsacion_2();
+		VICIntEnable = VICIntEnable | posicion_eint2;
+	}
+	else{
+		eint2_comprobar();}
+	//sino volver a pedir una alarma de 100ms
+}
+
 void eint1_ISR (void) __irq {
-	VICIntEnable = VICIntEnable | ~posicion_eint1; //se desactivan las solicitudes de eint1
+	eint1_count++;
+	VICIntEnClear = posicion_eint1;//se desactivan las solicitudes de eint1
+	VICVectAddr = 0; 
+	EXTINT = EXTINT | 2; //limpio la solicitud de interrupcion
 	nueva_pulsacion_eint1 = 1;
+	eint1_comprobar();
+	//envio una evento de alarma de 100ms q para q ejecute eint2_comprobar()
 }
 
 void eint2_ISR (void) __irq {
-	VICIntEnable = VICIntEnable | ~posicion_eint2; //se desactivan las solicitudes de eint1
-	nueva_pulsacion_eint2 = 1;
+	eint2_count++;
+	VICIntEnClear = posicion_eint2; //se desactivan las solicitudes de eint1
+	VICVectAddr = 0; 
+	EXTINT = EXTINT | 3; //limpio la solicitud de interrupcion
+	nueva_pulsacion_eint2 = 1;	
+	//envio una evento de alarma de 100ms q para q ejecute eint1_comprobar()
+	eint2_comprobar();
 }
 
 
+
+
+void boton1_estado(){
+	int entrada = button_nueva_pulsacion_1();
+	switch(eint1_estado)
+	{
+		case 'N': if (entrada == 1) {eint1_estado = 'P';}
+							else if (entrada == 0) {eint1_estado = 'N';}
+							break;
+		case 'P': if (entrada == 1) {eint1_estado = 'P';}
+							else if (entrada == 0) {eint1_estado = 'N';}
+							break;
+	}
+}
+
 void Gestor_Pulsacion_Init(){
-	PINSEL0 = 0xa0000000; //a activa eint1 y eint2
-	PINSEL1 = 0x00000000;
+	button_clear_nueva_pulsacion_1();
+	button_clear_nueva_pulsacion_2();
+	eint1_count = 0;
+	eint2_count = 0;
+	
+	PINSEL0 = PINSEL0 | 0xa0000000; //a activa eint1 y eint2
+	//PINSEL1 = 0x00000000;
 	
 	EXTMODE = EXTMODE | 0xF9; //set bit 1 and bit 2 to 0 -> eint1 is lvl-sensitive and eint2 is lvl-sensitive
 	EXTPOLAR = EXTPOLAR | 0xF9;  //set bit 1 and bit 2 to 0 -> eint1 is low-active and eint2 is low-active
