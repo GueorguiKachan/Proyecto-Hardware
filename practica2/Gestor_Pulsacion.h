8ad03b01911ec1e35e1ffc48a6b95d5a68b5c209
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-11-05 13:06:52 +0100

    Update Gestor_Pulsacion.h

--- practica2/Gestor_Pulsacion.h
+++ practica2/Gestor_Pulsacion.h
@@ -1,6 +1,6 @@
-
 #define EXTMODE          (*((volatile unsigned long *) 0xE01FC148))
 #define EXTPOLAR          (*((volatile unsigned long *) 0xE01FC14C))
+#define VICIntEnClear          (*((volatile unsigned long *) 0xFFFFF014))
 int button_nueva_pulsacion_1();
 
 int button_nueva_pulsacion_2();
@@ -12,3 +12,6 @@ void eint1_ISR (void) __irq ;
 void eint1_ISR (void) __irq ;
 
 void Gestor_Pulsacion_Init();
+
+void eint2_comprobar();
+void eint1_comprobar();
