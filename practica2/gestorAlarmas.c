8986523ced7b9a61553f410ff50f1c82ddfbddb2
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-11-05 12:01:11 +0100

    Update gestorAlarmas.c

--- practica2/gestorAlarmas.c
+++ practica2/gestorAlarmas.c
@@ -3,34 +3,34 @@
 #include "gestorAlarmas.h"
 
 struct arraysEventos{
-  int eventosProgramados[8];
-  int  tiempoEventos[8];
-  int estaLibre[8]
+  int eventosProgramados[8]; // Array con la info de los eventos (tipo de evento, si es periódico y los milisegundos de retardo)
+  int  tiempoEventos[8]; // Array para saber cuanto le falta a cada una de las alarmas
+  int estaLibre[8] // Array para saber cuales de las posiciones para las alarmas está libre u ocupada
   int numEventosProgramados;
-  int siguienteHueco;
 }
 
-int[] disparaEventos(){
+int[] disparaEventos(int tiempoTranscurrido){
   for(int i = 0;i<8;i++){
-    if(eventoProgramado[i]==1 ){ // Si había una alarma programada en este hueco se mira si devuelve el tiempo
+    if(estaLibre[i]==0 ){ // Si había una alarma programada en este hueco se mira si devuelve el tiempo
       
-      if(tiempoEventos[i]-(aqui iria el tiempo) <= 0){ // Si es menor que 0 generar evento
+      if(((tiempoEventos[i]-tiempoTranscurrido) <= 0) ){ // Si es menor que 0 generar evento
         // Mirar si es periódico o no, en el caso de que lo sea volver a programar
         int esPeriodico= (EventosProgramados[i] >> 23) & 0x1
         if(esPeriodico == 1){
           tiempoEventos[i] = eventosProgramados[i] & 0x007FFFF;
         }
         else{
-           tiempoEventos[i] -= tiempo periodico de interrupcion;
+          estaLibre[i] = 1;
         }
         
       }
       else{ // Si no se ha cumplido el tiempo, restar
-        
-      }
+        tiempoEventos[i] -= tiempoTranscurrido;
+      
     }
-    else if()
+    
   }
+    return algo;
 }
 
 void nuevoEvento(uint_t32 nuevoEvento){
@@ -42,8 +42,8 @@ void nuevoEvento(uint_t32 nuevoEvento){
   else{ // Hay que registrar nuevo evento
     if(numEventos < 8){
       for(int i=0; i<8;i++){ // Mirar donde hay un hueco libre
-        if(estaLibre[i] == 0){
-          estaLibre[i] = 1;
+        if(estaLibre[i] == 1){
+          estaLibre[i] = 0;
           eventosProgramados[i] = nuevoEvento;
           tiempoEventos[i] nuevoEvento & 0x007FFFFF;
           break;
