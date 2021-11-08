f7887db63e4c4bba3092d406d809c91d52e55aaf
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-11-09 00:34:04 +0100

    Update planificador.c

--- practica2/planificador.c
+++ practica2/planificador.c
@@ -1,11 +1,14 @@
 #include "planificador.h"
 #include "cola.h"
 #include "eventos.h"
+#include "Gestor_Energia.h"
+#include "Gestor_Pulsacion.h"
+#include "Gestor_Alarmas.h"
 
 
 void main(){
   // Programar alarma periódica para encolar un evento de tipo tempPeriodico
-  
+  temporizador_periodico(59); // 59+1/60MHz creo que son 1us !!!!!!!!!!!!!! Igual habría que hacer una función en gestor Alarmas para programar esto
   for{
     if(hayEventos()){ // Comprueba si en la cola hay eventos nuevos. Si los hay --> switch-case
       
@@ -15,11 +18,14 @@ void main(){
         case pulsacion1: 
         case pulsacion2:
         case tempPeriodico: // Establecer un temporizador periodico
-          disparaEventos(tiempoAlarma); //!!!!!!!!!!! Habría que poner el tiempo que se pone para la alarma periodica
+          disparaEventos(1000); // El temporizador interrumpe cada 1us = 1000ms (en el Gestor los tiempos de alarma se guardan en ms)
         case compruebaSuelta1: //Comprobar si se ha soltado la tecla 
           eint1_comprobar();
         case compruebaSuelta2:
           eint2_comprobar();
+      }
+      else{
+        PM_idle(); // Poner el procesador en modo reposo hasta que llegue la interrupción del temporizador
       }
     }
   }
