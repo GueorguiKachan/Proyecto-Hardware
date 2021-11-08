26726f904377e1f31474c4ea1598a9d7b311fbac
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-11-08 12:44:57 +0100

    Update Gestor_Alarmas.c

--- practica2/Gestor_Alarmas.c
+++ practica2/Gestor_Alarmas.c
@@ -1,6 +1,7 @@
 #include <LPC210X.H>                            // LPC21XX Peripheral Registers
 #include "timers.h"
 #include "Gestor_Alarmas.h"
+#include "cola.h"
 
 struct arraysEventos{
   int eventosProgramados[8]; // Array con la info de los eventos (tipo de evento, si es periódico y los milisegundos de retardo)
@@ -11,26 +12,24 @@ struct arraysEventos{
 
 void disparaEventos(int tiempoTranscurrido){
   for(int i = 0;i<8;i++){
-    if(estaLibre[i]==0 ){ // Si había una alarma programada en este hueco se mira si devuelve el tiempo
+    if(estaLibre[i]==0 ){ // Si había una alarma programada en este hueco se mira si ha llegado al tiempo programado
       
       if(((tiempoEventos[i]-tiempoTranscurrido) <= 0) ){ // Si es menor que 0 generar evento
-        // Mirar si es periódico o no, en el caso de que lo sea volver a programar
+        
         int esPeriodico= (EventosProgramados[i] >> 23) & 0x1
-        if(esPeriodico == 1){
+        if(esPeriodico == 1){ // Si es periodico se vuelve a programar la alarma
           tiempoEventos[i] = eventosProgramados[i] & 0x007FFFF;
         }
-        else{
+        else{ // Si no es periodico se libera espacio
           estaLibre[i] = 1;
         }
-        
+        // Encolar evento correspondiente   
       }
       else{ // Si no se ha cumplido el tiempo, restar
         tiempoEventos[i] -= tiempoTranscurrido;
       
     }
-    // Encolar evento correspondiente
   }
-    
 }
 
 void nuevoEvento(uint_t32 nuevoEvento){
@@ -38,6 +37,11 @@ void nuevoEvento(uint_t32 nuevoEvento){
   int tiempoNuevoEvento = nuevoEvento & 0x007FFFF;
   if(tiempoNuevoEvento == 0){
     int tipoEvento = (nuevoEvento >> 24)
+    for(int i=0; i<8;i++){
+      if(tipoEvento == (eventosProgramados[i] >> 24)){ // El tipo de evento coincide con el que se ha programado en ese hueco
+        estaLibre[i] = 1; // Cancelar la alarma
+      }
+    }
   }
   else{ // Hay que registrar nuevo evento
     if(numEventos < 8){
