dab680325bc2b3cead86bdd46bd954506992262c
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-11-11 09:47:33 +0100

    Update Gestor_Pulsacion.c

--- practica2/Gestor_Pulsacion.c
+++ practica2/Gestor_Pulsacion.c
@@ -11,118 +11,109 @@ const int posicion_eint1 = 0x00008000;
 const int posicion_eint2 = 0x00010000;
 
 
-int button_nueva_pulsacion_1(){
+static int button_nueva_pulsacion_1(){
   return nueva_pulsacion_eint1;
 }
 
-int button_nueva_pulsacion_2(){
+static int button_nueva_pulsacion_2(){
   return nueva_pulsacion_eint2;
 }
 
-void button_clear_nueva_pulsacion_1(){
+static void button_clear_nueva_pulsacion_1(){
   nueva_pulsacion_eint1 = 0;
 }
 
-void button_clear_nueva_pulsacion_2(){
+static void button_clear_nueva_pulsacion_2(){
   nueva_pulsacion_eint2 = 0;
 }
 
-void boton1_estado(){
+static bool boton1_estado(){
 	EXTINT = EXTINT | 2;
+	//si esta pulsado
 	if ((VICRawIntr & posicion_eint1) != posicion_eint1) {
 		VICIntEnClear = posicion_eint1;
-		//EXTINT = EXTINT | 2; //limpio la solicitud de interrupcion
-		nueva_pulsacion_eint1 = 1;}
+		nueva_pulsacion_eint1 = 1;
+	return true;}
+	//si no esta pulsado
 	else {
 		button_clear_nueva_pulsacion_1();
 		VICIntEnable |= posicion_eint1;
+		return false;
 	}
 }
 
-bool boton2_estado(){
+static bool boton2_estado(){
 	EXTINT = EXTINT | 3;
+	//si esta pulsado
 	if ((VICRawIntr & posicion_eint2) == posicion_eint2) {
 		VICIntEnClear = posicion_eint2;
-		//EXTINT = EXTINT | 3; //limpio la solicitud de interrupcion
 		nueva_pulsacion_eint2 = 1;
-		return true;}//true si esta pulsado
+		return true;}
+	//si no esta pulsado
 	else {
 		button_clear_nueva_pulsacion_2();
 		VICIntEnable |= posicion_eint2;
-		return false;//false si no esta pulsado
+		return false;
 	}
 }
 
 void eint1_comprobar(){
-	//si el boton no esta pulsado
-	if (boton2_estado()){eint2_comprobar();}
-	/*EXTINT = EXTINT | 2;
-	if ((VICRawIntr & posicion_eint1) == posicion_eint1){ 
-		VICVectAddr = 0;
-		button_clear_nueva_pulsacion_1();
-		VICIntEnable = VICIntEnable | posicion_eint1;
-	}else{
-	eint1_comprobar();}*/
-	//sino volver a pedir una alarma de 100ms
+	uint8_t evento;
+	uint32_t aux;
+	//si el boton sigue pulsado
+	if (boton1_estado()){
+		evento = alarmaSet;
+		aux = pulsacion1;
+		aux = aux << 24;
+		aux = aux | 100;
+		cola_guardar_eventos(evento, aux);
+	}
 }
 
 void eint2_comprobar(){
-	//si el boton no esta pulsado
-	if (boton2_estado()){eint2_comprobar();}
-	/*EXTINT = EXTINT | 3;
-	if ((VICRawIntr & posicion_eint2) != posicion_eint2){ 
-		
-		VICVectAddr = 0;
-		button_clear_nueva_pulsacion_2();
-		VICIntEnable = VICIntEnable | posicion_eint2;
+	uint8_t evento;
+	uint32_t aux;
+	//si el boton sigue pulsado
+	if (boton2_estado()){
+		evento = alarmaSet;
+		aux = pulsacion2;
+		aux = aux << 24;
+		aux = aux | 100;
+		cola_guardar_eventos(evento, aux);	
 	}
-	else{
-		eint2_comprobar();}
-	//sino volver a pedir una alarma de 100ms*/
 }
 
 void eint1_ISR (void) __irq {
+	uint8_t evento;
+	uint32_t aux;
+	
 	eint1_count++;
-	boton1_estado();
-	/*VICIntEnClear = posicion_eint1;//se desactivan las solicitudes de eint1
+	boton1_estado();	
 	
-	EXTINT = EXTINT | 2; //limpio la solicitud de interrupcion
-	nueva_pulsacion_eint1 = 1;*/
-	
-	
-	/*uint8_t evento = 0;
-	uint8_t aux = 1;
+	evento = alarmaSet;
+	aux = pulsacion1;
 	aux = aux << 24;
-	aux = aux | 100;*/
-	//cola_guardar_evento(evento, aux);
-	eint1_comprobar();
+	aux = aux | 100;
+	cola_guardar_eventos(evento, aux);
+	
 	VICVectAddr = 0; 
-	//envio una evento de alarma de 100ms q para q ejecute eint2_comprobar()
 }
 
 void eint2_ISR (void) __irq {
+	uint8_t evento;
+	uint32_t aux;
+	
 	eint1_count++;
 	boton2_estado();
-	//VICIntEnClear = posicion_eint1;//se desactivan las solicitudes de eint1
 	
-	//EXTINT = EXTINT | 3; //limpio la solicitud de interrupcion
-	//nueva_pulsacion_eint1 = 1;
-	
-	
-/*	uint8_t evento = alarmaSet;
-	uint8_t aux = pulsacion2;
+	evento = alarmaSet;
+	aux = pulsacion2;
 	aux = aux << 24;
-	aux = aux | 100;*/
-	//cola_guardar_evento(evento, aux);
-	eint2_comprobar();
-	VICVectAddr = 0; 
+	aux = aux | 100;
+	cola_guardar_eventos(evento, aux);
 	
-	//envio una evento de alarma de 100ms q para q ejecute eint2_comprobar()
+	VICVectAddr = 0; 
 }
-
-
-
-
 
 
 void Gestor_Pulsacion_Init(){
