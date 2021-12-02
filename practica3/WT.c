e22a3ecdca06b3a8587f7b179d4b540bd4923bda
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2021-12-02 01:24:39 +0100

    Add files via upload

--- practica3/WT.c
+++ practica3/WT.c
@@ -1,5 +1,6 @@
 #include <LPC210X.H>
 #include "WT.h"
+#include "timers.h"
 
 void WD_init(int sec){
 
@@ -7,7 +8,11 @@ void WD_init(int sec){
 
 void WD_feed(){ 
 	// Antes de escribir el AA hay que desactivar las interrupciones
+	disable_isr();
+	disable_isr_fiq();
 	WDFEED = 0xAA;						   
-  WDFEED = 0x55;  
+  WDFEED = 0x55;
+  enable_isr_fiq();	
+	enable_isr ();
 	// Volver a activar las interrupciones
 }
