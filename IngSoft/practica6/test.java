f20397567896816ab315d87c6d7ee3f0d8e5411b
Ernesting <77130942+Ernesting@users.noreply.github.com>
2021-12-11 12:02:51 +0100

    Create test.java

---
+++ IngSoft/practica6/test.java
@@ -0,0 +1,43 @@
+package es.unizar.eina.notepadv3;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Test {
+    private final NotesDbAdapter notes;
+    public Test(NotesDbAdapter notes) {
+        this.notes = notes;
+    }
+
+    public void pruebaVolumen(){
+        String pre = new String();
+        pre = "PRE-";
+        for (int i = 0; i <1000; i++){
+            notes.createNote(pre + i,"gueorgui");
+        }
+    }
+
+    public void deshacerPruebaVolumen(){
+        String pre = new String();
+        pre = "PRE-";
+        for (int i = 0; i <1000; i++){
+            notes.deleteNoteByTitle(pre + i);
+        }
+    }
+
+    public void pruebaSobreCarga(){
+        String texto = "texto";
+        int tamanyo = 5;
+       // Log.v("PruebaSobrecarga", "tamanyo="+ tamanyo);
+        /*while (true){
+            Log.v("PruebaSobrecarga", "tamanyo="+ tamanyo);
+            notes.createNote("sobrecarga - "+ tamanyo, texto);
+            tamanyo += 5;
+            texto += texto;
+        }*/
+    }
+}
