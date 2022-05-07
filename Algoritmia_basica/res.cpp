87526bbe8fe59157bd5f014414f7e72e3b217856
Ernesting <77130942+Ernesting@users.noreply.github.com>
2022-05-07 22:36:31 +0200

    Update res.cpp

--- Algoritmia_basica/res.cpp
+++ Algoritmia_basica/res.cpp
@@ -3,16 +3,24 @@
 #include <fstream>
 #include <sstream>
 #include <unistd.h>
+#include <map>
+#include <string.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 int offset = 0;
 string A = "ABBC";
 string B = "BABB";
-enum class Transf { borrar, insert, sustit, nada };
+enum Transf { borrar, insert, sustit, nada };
 
+Transf** T  = nullptr;
+int size = 0;
+int capacity = 0; 
 
-
-int C[5][5];
+//int C[5][5];
 
 int min( int a, int b, int c){
     if (a <= b && a <= c) {
@@ -24,8 +32,8 @@ int min( int a, int b, int c){
 }
 
 
-Transf T[4][4];
-
+//Transf T[4][4];
+/*
 void compSec(){
     int i, j, x, y, z;
     for (i = 0; i < 5; i++){
@@ -55,30 +63,11 @@ void compSec(){
             }
         }
     }
-}
+}*/
 
 
-void res( int i, int j ){
-    int k;
 
-    if (i == 0){
-        for (k = 0; k < j; k++){
-            cout << "Anadir" << B[k]<< "en el lugar" << k<<endl;
-        }
-    }else if (j == 0){
-        for (k = 0; k < i; k++){
-            cout << "Borrar car numero " << k << endl;
-        }
-    }else{
-        switch (T[i][j]){
-            case Transf::borrar: res(i-1, j); cout << "borrar " <<i <<endl; break;
-            case Transf::insert: res(i, j-1);cout<< "insert " << i <<" : "<<B[j-1] <<endl; break;
-            case Transf::sustit: res(i-1,j-1);cout<< "sustit " << i << " de A por numero "<< j<<" de B" <<endl; break;
-            case Transf::nada: res(i-1, j-1); break;
-        }
-    }
-}
-
+/*
 void deshacer(string ultimaVersion, string ficheroBk, string versionRestaurada){
     ifstream fin1; fin1.open(ultimaVersion);
     ifstream fin2; fin2.open(ficheroBk);
@@ -134,10 +123,142 @@ void deshacer(string ultimaVersion, string ficheroBk, string versionRestaurada){
 
     fin1.close(); fin2.close(); fout.close();
 
+}*/
+
+void mostrarT (){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j <3; j++){
+            cout << T[j][i]<<endl;
+        }
+    }
 }
 
-int main (){
-    string fichero;
+static void res( int i, int j){
+    cout << i <<":"<<j<<endl;
+    int k;
+
+    if (i == 1){
+        for (k = 0; k < j; k++){
+            cout << "Anadir" << B[k]<< "en el lugar" << k<<endl;
+        }
+    }else if (j == 1){
+        for (k = 0; k < i; k++){
+            cout << "Borrar car numero " << k << endl;
+        }
+    }else{
+        switch (T[i-1][j-1]){
+            case borrar: res(i-1, j); cout << "borrar " <<i <<endl; break;
+            case insert: res(i, j-1);cout<< "insert " << i <<" : "<<B[j-1] <<endl; break;
+            case sustit: res(i-1,j-1);cout<< "sustit " << i << " de A por numero "<< j<<" de B" <<endl; break;
+            case nada: res(i-1, j-1); cout<<"nada"<<endl; break;
+            default: cout <<"mal"<<endl;
+        }
+    }
+}
+
+void inicializarT (int filas, int columnas){
+    T = new Transf*[filas];
+    for (int i = 0; i < filas; i++){
+        T[i] = new Transf[columnas];
+    }
+}
+
+void borrarT (int filas){
+    for(int i = 0; i < filas; i++){
+        delete[] T[i];
+        delete[] T;
+    }
+}
+
+
+
+
+void hallarCambios(string ficheroEntrada, string ficheroSalida){
+	ifstream t(ficheroEntrada);
+	stringstream buffer;
+	buffer << t.rdbuf();
+
+	string sEntrada = buffer.str();
+
+	ifstream s(ficheroSalida);
+	std:stringstream buffer2;
+	buffer2 << s.rdbuf();
+
+	string sSalida = buffer2.str();
+
+	const int tamanyoSalida = sSalida.size();
+	const int tamanyoEntrada = sEntrada.size();
+	//Transf T[tamanyoEntrada][tamanyoSalida];
+    
+    inicializarT(tamanyoEntrada -1,tamanyoSalida-1);
+    cout << "Se ha inicializado " <<endl;
+	int C[tamanyoEntrada][tamanyoSalida];
+
+	for (int i = 0; i < tamanyoSalida; i++){
+		C[i][0] = i;
+        cout << " i: " <<i <<endl;
+	}
+	for (int j = 0; j < tamanyoEntrada; j++){
+		C[0][j] = j;
+        cout << " j: " <<j <<endl;	
+	}
+
+	int x,y,z;
+
+	for(int i=1; i < tamanyoSalida; i++){
+		//cout << "Letra i " << sSalida[i] << endl;
+		for(int j=1; j < tamanyoEntrada; j++){
+			//cout << "Letra j " << sEntrada[j] << endl;
+			x = C[i-1][j] + 1;
+			y = C[i][j-1] + 1;
+			if(sEntrada[j-1] == sSalida[i-1]){
+				z = C[i-1][j-1];
+			}else{
+				z = C[i-1][j-1] + 1;
+			}
+			int minimo = min(x,y,z);
+			C[i][j] = minimo;
+			if(minimo == x){ // (min=x) => borrar 
+				//string xd =  "b";
+				//xd.append(to_string(i));
+				//cout << "Minimo x-" << xd  << endl;
+                cout << "borrar" <<endl;
+				T[i-1][j-1] = Transf::borrar;
+			}else if(minimo == y){ // (min=y) => insert
+				//string xd =  "i";
+				//xd.append(to_string(i));
+				//xd += ":";
+				//xd += sEntrada[i];// xd.append(sEntrada[i]);
+				// cout << "Minimo y - "<< xd << endl;
+                cout << "insert" <<endl;
+				T[i-1][j-1] = Transf::insert;
+			}else if(minimo == z && sEntrada[i-1] == sSalida[j-1]){ // (min=z) & A[i]=B[j] => nada
+				//cout << "Minimo z- n" << endl;
+                cout << "nada" <<endl;
+				T[i-1][j-1] = Transf::nada;	
+			}else{ // (min=z) & A[i]!=B[j] => sustit
+				//string xd =  "s";
+				//xd.append(to_string(i));
+				//xd += ":";
+				//xd += sEntrada[i];
+				//cout << "Minimo z- " << xd << endl;
+                cout << "sustit" <<endl;
+				T[i-1][j-1] = Transf::sustit;
+			}	
+		}
+	}
+    cout << "antes de res() "<<endl;
+    mostrarT();
+    res(tamanyoEntrada -1, tamanyoSalida-1);
+    borrarT(tamanyoEntrada-1 );
+}
+
+
+int main(int argc, char *argv[]){
+	hallarCambios(argv[1], argv[2]);
+}
+
+    /*string fichero;
     string comando;
     cout << "Inserte fichero a tratar versiones: "<<endl;
     cin >> fichero;
@@ -152,10 +273,10 @@ int main (){
               //  }
           //  }
           break;
-    }
-    compSec();
-    res(4,4);
-    deshacer("a.txt", "c.txt", "b.txt");
-}
+    }*/
+    //compSec();
+    //res(4,4);
+    //deshacer("a.txt", "c.txt", "b.txt");
+
 
 
